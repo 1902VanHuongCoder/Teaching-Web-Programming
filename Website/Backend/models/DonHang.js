06c9b98da3eb01cf75fa36a4e4e36324a14dc935
vanhuong <tovanhuong008@gmail.com>
2025-09-16 16:03:07 +0700

    Tạo Model cho đơn hàng và Sửa tên Video+35 thành Video+36

---
+++ Website/Backend/models/DonHang.js
@@ -0,0 +1,81 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/mysql-config.js";
+import Sach from "./Sach.js";
+// Định nghãi model DonHang
+const DonHang = sequelize.define('DonHang', {
+    donHangID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,   
+        autoIncrement: true 
+    },
+    nguoiDungID: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tenKhachHang: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, soDienThoaiKH: {
+        type: DataTypes.STRING,
+        allowNull: false    
+    }, 
+    ngayDat: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }, 
+    tongTien: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }, 
+    trangThai: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    diaChiGiaoHang: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ghiChu: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    }
+}, {
+    timestamps: true, // Tự động thêm createdAt và updatedAt fields
+    tableName: 'don-hang'
+});
+
+
+// Bảng trung gian DonHang_Sach cho quan hệ nhiều nhiều bao gồm 4 trường chính: donHangID, sachID, soLuong, donGia 
+export const DonHang_Sach = sequelize.define('DonHang_Sach', {
+    donHangID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    sachID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    soLuong: {  
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    donGia: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+    tableName: 'don-hang_sach'
+});
+
+// Thiết lập quan hệ nhiều nhiều giữa DonHang và Sach thông qua bảng trung gian DonHang_Sach 
+DonHang.belongsToMany(Sach, { through: DonHang_Sach, foreignKey: 'donHangID' });
+
+Sach.belongsToMany(DonHang, { through: DonHang_Sach, foreignKey: 'sachID' });
+
+
+export default DonHang; 
+
+
+
+
