361b3b0435a496046ff1b9da477f977da8edf65d
vanhuong <tovanhuong008@gmail.com>
2025-09-26 14:54:01 +0700

    Đổi trạng thái nút Đăng nhập đăng xuất

--- Website/Frontend/src/components/Navigation.jsx
+++ Website/Frontend/src/components/Navigation.jsx
@@ -1,7 +1,25 @@
+import { useContext } from "react";
 import logo from "../assets/Red Panda.png";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { UserContext } from "../contexts/user-context";
 function Navigation() {
+  // Sử dụng giá trị user từ context
+  const { user, setUser } = useContext(UserContext);
+
+
+  const handleDangXuat = () => {
+     // Xóa dữ liệu người dùng ra khỏi localstorage
+      localStorage.removeItem("user"); 
+
+     // Xóa dữ liệu người dùng lưu trong biến user
+      setUser(null);
+
+     // Chuyển hướng về trang đăng nhập
+      window.location.href = "/dangnhap"; 
+  }
+
+
   return (
     <nav className="px-6 py-4 flex justify-between items-center bg-[#00809D] text-white text-sm border-b-[1px] border-b-[rgba(255,255,255,.2)]">
       <div className="flex items-center space-x-12">
@@ -29,12 +47,15 @@ function Navigation() {
           <FaShoppingCart className="h-6 w-6" />
         </Link>
         <div className="space-x-0">
-          <Link to="/dangnhap">
-            <button className="bg-white text-black px-4 py-2 rounded-full font-semibold">
-              Đăng nhập
-            </button>
-          </Link>
-          <button className="hidden">Đăng xuất</button>
+          {user ? (
+            <button onClick={handleDangXuat} className="">Đăng xuất</button>
+          ) : (
+            <Link to="/dangnhap">
+              <button className="bg-white text-black px-4 py-2 rounded-full font-semibold">
+                Đăng nhập
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </nav>
